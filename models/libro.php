0f5439c81c7d93613797695091ee8aa27d707a33
P-4L-0 <105886687+P-4L-0@users.noreply.github.com>
2025-03-23 14:56:57 -0600

    feat: new models and controllers

---
+++ models/libro.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . '/../database/connection.php';
+
+class Libro{
+
+    private PDO $db;
+    public function __construct(){
+        $this->db = Connection::connection();
+    }
+
+    public function create(){
+
+    }
+
+    public function getOne(){
+
+    }
+
+    public function getAll(){
+
+    }
+
+    public function __destruct(){
+        Connection::disconnect();
+    }
+
+
+}
+
+
+?>
